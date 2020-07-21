09b0a736e5e77ef87a1b5ed5e32174e031d1e99a
TAYBI <taybib4@gmail.com>
2020-07-21 11:59:15 +0100

    Js for carousel

---
+++ 07 Carousel/index.js
@@ -0,0 +1,27 @@
+let left = document.querySelector(".left");
+let right = document.querySelector(".right");
+let card = document.querySelectorAll(".card");
+let cards = document.querySelector(".cards");
+let cards_Class = document.getElementsByClassName("card");
+let numCard = card.length;
+let index = 3;
+
+function move_card(x) {
+  for (let i = 0; i < cards_Class.length; i++) {
+    const _card = cards_Class[i];
+    _card.style.transform = `translateX(${x}px)`;
+  }
+}
+
+left.addEventListener("click", () => {
+  if (index <= numCard) move_card(-290);
+  console.log("left");
+  index++;
+});
+
+right.addEventListener("click", () => {
+  if (index != 2) move_card(290);
+  index--;
+  // card left
+  console.log("right");
+});
